150f3d66fa2cae280b3a4e4f639d74374a32202d
sharathp <chandra.ps@gmail.com>
2016-07-25 23:36:26 -0400

    Initial commit

---
+++ app/src/main/java/com/sharathp/myorktimes/ArticleListActivity.java
@@ -0,0 +1,13 @@
+package com.sharathp.myorktimes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ArticleListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_article_list);
+    }
+}
